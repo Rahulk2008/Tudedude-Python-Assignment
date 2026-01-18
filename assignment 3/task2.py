fba5ddcacb5f95a1293d5f1706b309c570cfd150
Rahulk2008 <rahulkamble7556@gmail.com>
2026-01-18 20:28:54 +0530

    Add files via upload

---
+++ assignment 3/task2.py
@@ -0,0 +1,10 @@
+import math
+
+a = float(input("Enter a number :"))
+sqrt_a = math.sqrt(a)
+nlog_a = math.log(a)
+sine_a = math.sin(a)
+
+print(f"Square root  : {sqrt_a}")
+print(f"Logarithm  : {nlog_a}")
+print(f"Sine  : {sine_a}")
